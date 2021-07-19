cf6c26197e8a82c2a5f2c2d6af447a3a2c6eba29
Bruno Noveli <brunimzuppwolf@gmail.com>
2021-07-19 15:59:09 -0300

    Implementando movimentos possíveis da Torre(Rook)

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -19,6 +20,54 @@ public class Rook extends ChessPiece {
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        // Posição auxiliar
+        Position p = new Position(0,0);
+
+        // Acima
+        p.setValues(position.getRow() - 1, position.getColumn());
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getRow() - 1);
+        }
+        // Verificando se na posição que deu falso é uma peça adversária
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // Esquerda
+        p.setValues(position.getRow(), position.getColumn() - 1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setColumn(p.getColumn() - 1);
+        }
+        // Verificando se na posição que deu falso é uma peça adversária
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // Direita
+        p.setValues(position.getRow(), position.getColumn() + 1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setColumn(p.getColumn() + 1);
+        }
+        // Verificando se na posição que deu falso é uma peça adversária
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // Abaixo
+        p.setValues(position.getRow() + 1, position.getColumn());
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getRow() + 1);
+        }
+        // Verificando se na posição que deu falso é uma peça adversária
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
         return mat;
     }
 }
